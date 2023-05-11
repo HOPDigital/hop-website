8aa533b7444c7c8f0f1d40374b227b2a25c14258
Matt <matheusc039@hotmail.com>
2023-05-11 12:06:41 -0300

    feat: custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
         "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                white_hop: "#FFFFFF",
+                pale_hop: "#F4F5F7",
+                grey_hop: "#DCDFE6",
+                red_hop: "#EB3254",
+                pink_hop: "#DD226D",
+                purple_hop: "#8C4CF5",
+                blur_hop: "#121632",
+                black_hop: "#1E1E1E",
+            },
+        },
     },
     plugins: [],
 }
