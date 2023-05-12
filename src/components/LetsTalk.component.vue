c1892fe949e8ed6549e0b8b66023e0355119294b
Matt <matheusc039@hotmail.com>
2023-05-12 17:30:33 -0300

    feat: let's talk responsive component

---
+++ src/components/LetsTalk.component.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="flex flex-col justify-center items-center bg-gradient-to-r from-purple_hop to-pink_hop py-14 lg:py-20">
+        <h1 class="text-2xl md:text-4xl text-white font-extrabold">Que tal trocar uma ideia?</h1>
+
+        <button type="button" class="border-none rounded-md bg-purple_hop text-white font-normal p-3 mt-5 w-[50%] xl:w-[10%]">
+            Vamos conversar
+        </button>
+    </section>
+</template>
