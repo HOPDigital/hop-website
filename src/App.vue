c856c7fbcbc26b75da2d3df4ffe441d5bb9857ba
Matt <matheusc039@hotmail.com>
2023-05-12 17:30:11 -0300

    refactor: adding components to the app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import Introduction from './components/Introduction.vue';
+import Introduction from './components/Introduction.component.vue';
 import TipSection from './components/TipSection.vue';
-import BuildingProduct from './components/BuildingProduct.vue';
-import Skills from "./components/Skills.vue";
+import BuildingProduct from './components/BuildingProduct.component.vue';
+import Skills from "./components/Skills.component.vue";
 import WorkWithUs from "./components/WorkWithUs.vue";
+import LetsTalk from './components/LetsTalk.component.vue';
 </script>
 
 <template>
@@ -11,16 +12,16 @@ import WorkWithUs from "./components/WorkWithUs.vue";
 
     <TipSection>
         <template v-slot:content>
-            <main class="flex flex-col py-36 justify-center items-center gap-7">
-                <span class="uppercase text-pink_hop font-semibold text-sm">
+            <main class="flex flex-col py-36 px-8 justify-center items-center gap-7">
+                <span class="uppercase text-pink_hop font-semibold text-xs md:text-sm">
                     Precisa de desenvolvedores de primeira linha?
                 </span>
 
-                <h2 class="text-[48px] font-extrabold text-white">
+                <h2 class="text-4xl lg:text-[48px] font-extrabold text-white text-center">
                     Você está no lugar certo
                 </h2>
 
-                <p class="text-white w-[30%] text-center leading-[32px]">
+                <p class="text-white lg:w-[30%] text-left md:text-center leading-[32px]">
                     A Hop ajuda empreendedores a construir e lançar novos produtos e,
                     ajuda as empresas em crescimento conectando equipes de desenvolvimento pronta para uso e começar a
                     construir.
@@ -32,6 +33,8 @@ import WorkWithUs from "./components/WorkWithUs.vue";
     <BuildingProduct />
 
     <Skills />
+
+    <LetsTalk/>
 
     <WorkWithUs />
 
