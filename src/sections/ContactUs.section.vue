f7004cb78c3c74f43b6a53a43b58baeccc1ef284
Matt <matheusc039@hotmail.com>
2023-05-13 16:13:58 -0300

    feat: contact us responsive section

---
+++ src/sections/ContactUs.section.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="flex flex-col justify-center items-center bg-blur_hop w-full py-24">
+        <h1 class="text-3xl lg:text-[40px] text-purple_hop font-extrabold">
+            Fale conosco
+        </h1>
+
+        <div class="flex flex-row justify-center items-center mt-10">
+            <input 
+                class="bg-white_hop px-4 py-3 text-sm text-purple_hop placeholder:text-purple_hop rounded-l-md outline-none drop-shadow-md"
+                placeholder="Digite seu e-mail aqui"
+                type="email"
+            />
+
+            <button 
+                class="bg-pink_hop text-sm text-white_hop font-semibold px-4 py-3 rounded-r-md drop-shadow-md"
+                type="submit"
+            >
+                Enviar
+            </button>
+        </div>
+    </section>
+</template>
