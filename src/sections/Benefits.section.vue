12b81ddbfe8a663ac148d245221eaf3bb7a8df63
Matt <matheusc039@hotmail.com>
2023-05-18 21:01:17 -0300

    refactor: tbc content

--- src/sections/Benefits.section.vue
+++ src/sections/Benefits.section.vue
@@ -1,32 +1,37 @@
 <script>
-    import { RiThumbUpLine, RiExchangeDollarLine, RiGitPullRequestLine } from "vue-remix-icons";
+import { RiThumbUpLine, RiExchangeDollarLine, RiGitPullRequestLine } from "vue-remix-icons";
 
-    export default {
-        data() {
-            return {
-                workDetails: [
-                    {
-                        id: 1,
-                        icon: RiThumbUpLine,
-                        title: "Respire facilmente",
-                        description: "Com nosso processo de desenvolvimento plug-and-play",
-                    },
-                    {
-                        id: 2,
-                        icon: RiExchangeDollarLine,
-                        title: "TBC",
-                        description: "TBC",
-                    },
-                    {
-                        id: 3,
-                        icon: RiGitPullRequestLine,
-                        title: "Construa mais inteligente",
-                        description: "Ajudamos a estreitar os recursos com base no valor comercial do produto",
-                    },
-                ]
-            }
+export default {
+    data() {
+        return {
+            workDetails: [
+                {
+                    id: 1,
+                    icon: RiThumbUpLine,
+                    title: "Respire facilmente",
+                    description: "Muita pressão? Precisando de um espaço? Deixa com a gente",
+                },
+                {
+                    id: 2,
+                    icon: RiExchangeDollarLine,
+                    title: "TBC",
+                    description: "TBC",
+                },
+                {
+                    id: 3,
+                    icon: RiGitPullRequestLine,
+                    title: "Construa mais inteligente",
+                    description: "Ajudamos a estreitar os recursos com base no valor comercial do produto",
+                },
+            ]
         }
+    },
+    components: {
+        RiThumbUpLine,
+        RiExchangeDollarLine,
+        RiGitPullRequestLine
     }
+}
 </script>
 
 <template>
@@ -37,24 +42,52 @@
             </h1>
 
             <p class="text-white text-sm md:text-[18px]">
-                Você não tem apenas desenvolvedores trabalhando em seu time. 
+                Você não tem apenas desenvolvedores trabalhando em seu time.
                 Você também tem todos os recursos de que seu time precisa para tornar seu produto um sucesso.
             </p>
         </div>
 
         <div class="flex flex-col lg:flex-row justify-center items-center w-full my-20 gap-y-14 gap-x-32">
-            <div 
-                class="flex flex-col justify-center bg-darker_blur_hop p-6 w-[318px] h-[360px] drop-shadow-xl"
-                v-for="details in workDetails"
-                :key="details.id"
-            >
-                <h4 class="text-purple_hop uppercase font-semibold text-left">
-                    {{  details.title }}
-                </h4>
+            <div class="flex flex-col justify-evenly bg-darker_blur_hop p-6 w-[318px] h-[360px] drop-shadow-xl">
+                <RiThumbUpLine class="fill-pink_hop h-[50px]" />
 
-                <p class="text-white p-2 mt-4 leading-[32px]">
-                    {{  details.description }}
-                </p>
+                <div>
+                    <h4 class="text-purple_hop uppercase font-semibold text-left">
+                        TBC
+                    </h4>
+
+                    <p class="text-white p-2 mt-4 leading-[32px]">
+                        Soon
+                    </p>
+                </div>
+            </div>
+
+            <div class="flex flex-col justify-evenly bg-darker_blur_hop p-6 w-[318px] h-[360px] drop-shadow-xl">
+                <RiExchangeDollarLine class="fill-pink_hop h-[50px]" />
+
+                <div>
+                    <h4 class="text-purple_hop uppercase font-semibold text-left">
+                        TBC
+                    </h4>
+
+                    <p class="text-white p-2 mt-4 leading-[32px]">
+                        Soon
+                    </p>
+                </div>
+            </div>
+
+            <div class="flex flex-col justify-evenly bg-darker_blur_hop p-6 w-[318px] h-[360px] drop-shadow-xl">
+                <RiGitPullRequestLine class="fill-pink_hop h-[50px]" />
+
+                <div>
+                    <h4 class="text-purple_hop uppercase font-semibold text-left">
+                        TBC
+                    </h4>
+
+                    <p class="text-white p-2 mt-4 leading-[32px]">
+                        Soon
+                    </p>
+                </div>
             </div>
         </div>
     </section>
